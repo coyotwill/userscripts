8ab7ea53899c0ccfe1bab45534e242b74c1d0d69
coyotwill <13336678+coyotwill@users.noreply.github.com>
2019-08-29 18:15:58 -0400

    Add ROLL option

--- jiraboardtimer.js
+++ jiraboardtimer.js
@@ -7,8 +7,8 @@
 // ==/UserScript==
 
 // Test values, to be adjusted for your own use-case
-var glbWarnValueSec = 5;
-var glbOverValueSec = 10;
+var glbWarnValueSec = 90;
+var glbOverValueSec = 120;
 
 var glbOkColor = "lightgreen";
 var glbWarnColor = "khaki";
@@ -16,6 +16,7 @@ var glbOverColor = "tomato";
 
 var gblTimerStart;
 var gblTimerId;
+var glbInterval;
 
 function glbUpdateStatus(elt, speaker) {
     var elapsed = ((performance.now() - gblTimerStart) / 1000);
@@ -36,10 +37,32 @@ function glbStopStatus(elt) {
     elt.css("background-color", "initial");
 }
 
+function glbShuffle(list, insertTgt) {
+    insertTgt = insertTgt || list.first().prev();
+    // Randomize list
+    list.sort(function(){
+        return Math.random() - 0.5;
+    }).detach().insertAfter(insertTgt); // Detach and reinsert to update dom.
+}
+
+function glbRoll(list, insertTgt) {
+    setTimeout(function(){
+        glbShuffle(list, insertTgt);
+        glbInterval = glbInterval * 115 / 100
+        if(glbInterval<400) {
+            glbRoll(list, insertTgt)
+        }
+    }, glbInterval);
+}
+
 $("#content").before(`
     <div id="tmStopWatchDiv">
         <p id="tmTimeSpan">&nbsp;</p>
     </div>
+`);
+
+$("#ghx-view-presentation").prepend(`
+    <button id="tmTimeShuf" class="aui-button">&#127922 Roll</button>
 `);
 
 $(".js-quickfilter-button").click(zEvent => {
@@ -62,6 +85,16 @@ $(".js-quickfilter-button").click(zEvent => {
     }
 } );
 
+$("#tmTimeShuf").click(zEvent => {
+    var elt = $("#js-work-quickfilters");
+    // Get all quick-filters starting with a Letter
+    var list = elt.children("dd:not(.ghx-quickfilter-trigger)").filter(function() {
+        return /^[A-Za-z]/.test($(this).text())
+    })
+    glbInterval = 40;
+    glbRoll(list);
+} );
+
 GM_addStyle(`
 #tmTimeSpan {
     padding: 5px;
@@ -69,9 +102,8 @@ GM_addStyle(`
     font-size: 2em;
 }
 #tmTimeSpan::before {
-  content: "\u23F1 "; // StopWatch emoji
+    content: "\u23F1 "; // StopWatch emoji
 }
-
 #tmStopWatchDiv {
     clear: both;
     border-bottom: solid 1px lightgrey;
